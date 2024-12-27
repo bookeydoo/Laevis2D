97f6b6a1fed5400e905da1fddae1d2824e372dbd
Waleed Amgad <141445359+bookeydoo@users.noreply.github.com>
2024-12-27 03:58:47 +0200

    Delete src/main/java/Laevis/LaevisUtilities directory

--- src/main/java/Laevis/LaevisUtilities/Time.java
+++
@@ -1,9 +0,0 @@
-package LaevisUtilities;
-
-import static org.lwjgl.glfw.GLFW.glfwGetTime;
-
-public class Time {
-    public static float GetTime() {
-        return (float) glfwGetTime();
-    }
-}
